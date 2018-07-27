8e6c27c6bea0b6a092fa3d6e2620eff469cc40ce
Okarim <omark.pineda@gmail.com>
2018-07-27 13:50:43 -0500

    Antifragilidad minimos

--- rbn.py
+++ rbn.py
@@ -4,9 +4,10 @@ import numpy as np
 from scipy.spatial.distance import hamming
 import multiprocessing
 from functools import partial
+from  tqdm import tqdm
 from  tqdm import trange
 
-class RBN:    
+class RBN:
     def CreateNet(self, K, N, p):
         """
         K = number of connections
@@ -39,7 +40,7 @@ class RBN:
         Bool= lookup table
         T = timesteps
         initial = initial state (random if empty)
-        M = how many perturbations
+        X = how many perturbations
         O = how often the perturbations take place
         """
         Pow = 2**np.arange(np.size(self.Con, 1)) # [ 1 2 4 ... ], for converting inputs to numerical value
@@ -138,7 +139,7 @@ class RBN:
         for j in range(runs):
             initial = np.random.randint(0, 2, self.N)
             State=self.RunNet(T, initial)
-            C0 = entropia(State)
+            C0 = complexity(State)
             f+=pool.map(partial(self.func, T=T, initial=initial, O=O, C0=C0), range(1, int(self.N/2)+1))
         f/=runs # average fragility by perturbation
         pool.close()
@@ -147,11 +148,11 @@ class RBN:
     def func(self, i, T, initial, O, C0):
         f=np.zeros(int(self.N/2))
         State=self.RunNet(T, initial, i, O)
-        C = entropia(State)
+        C = complexity(State)
         f=fragility(C, C0, i, O, self.N, T)
         return f
 
-def entropia(state):
+def complexity(state):
     """
     Measuring Complexity Based on Shanon Entropy 
     state = matrix of a RBN states
@@ -167,6 +168,14 @@ def entropia(state):
     return C
 
 def fragility(C, C0, X, O, N, T):
+    """
+    C0 = initial complexity
+    C = final complexity
+    X = how many perturbations
+    O = how often the perturbations take place
+    N = number of nodes, indexed 0 .. N-1
+    T = timesteps
+    """
     dx =(X*(T/O))/(N*T) # degree of perturbation
     sigma = np.mean(C-C0) # degree of satisfaction
     return -sigma*dx
@@ -174,15 +183,16 @@ def fragility(C, C0, X, O, N, T):
 def plots(red, T, M, O):
     initial = np.random.randint(0, 2, red.N)
     State=red.RunNet(T, initial)
-    plt.imshow(State, cmap='Greys', interpolation='None', aspect='auto')
+    plt.imshow(State, cmap='Greys', interpolation='None')
+    #, aspect='auto'
     plt.show()
-    C1 = entropia(State)
+    C1 = complexity(State)
     print("Complejidad: "+str(np.mean(C1)))
     initial2 = np.random.randint(0, 2, red.N)
     State2=red.RunNet(T, initial2)
-    plt.imshow(State2, cmap='Greys', interpolation='None', aspect='auto')
+    plt.imshow(State2, cmap='Greys', interpolation='None')
     plt.show()
-    C3 = entropia(State2)
+    C3 = complexity(State2)
     print("Complejidad: "+str(np.mean(C3)))
     print("\nDistancia inicial: ")
     print(hamming(State[0], State2[0])) #distance
@@ -190,15 +200,15 @@ def plots(red, T, M, O):
     print(hamming(State[T-1], State2[T-1])) #distance
     
     State=red.RunNet(T, initial, M, O)
-    plt.imshow(State, cmap='Greys', interpolation='None', aspect='auto')
+    plt.imshow(State, cmap='Greys', interpolation='None')
     plt.show()
-    C2 = entropia(State)
+    C2 = complexity(State)
     print("Complejidad: "+str(np.mean(C2)))
     print("Fragilidad: "+str(fragility(C2, C1, M, O, red.N, T))) 
     State2=red.RunNet(T, initial2, M, O)
-    plt.imshow(State2, cmap='Greys', interpolation='None', aspect='auto')
+    plt.imshow(State2, cmap='Greys', interpolation='None')
     plt.show()
-    C4 = entropia(State2)
+    C4 = complexity(State2)
     print("Complejidad: "+str(np.mean(C4)))
     print("Fragilidad: "+str(fragility(C4, C3, M, O, red.N, T)))
     print("\nDistancia inicial: ")
@@ -208,17 +218,19 @@ def plots(red, T, M, O):
     return
 
 def plotAntifragile(N, p, T):
+    """
+    Plots the antifragility of RBNs varying perturbations
+    """
     number_of_iterations=50
-    
+    FR= np.zeros((5, int(N/2)))
     for K in range(1, 6):
         f=np.zeros(( number_of_iterations, int(N/2) ))
         i=0
         for x in trange(number_of_iterations):
             red=RBN()
-            red.CreateNet(K, N, p)
+            red.CreateNet(float(K), N, p)
             f[i]=red.antifragile(T, runs=10)
             i+=1
-            
         plt.figure()
         g1=np.mean(f, 0)
         plt.plot(g1)
@@ -235,6 +247,58 @@ def plotAntifragile(N, p, T):
         plt.show()
         plt.show()
         
+        FR[K-1]=g1
+    
+#    fig = plt.figure()
+#    ax = fig.add_subplot(111, projection='3d')
+#    ax.plot_surface(FR[:, 0], FR[:, 1], )
+#    plt.show()
+    
+    return FR
+
+def minAntifragile(red, runs=50):
+    """
+    Plots the minimum fragility for each K against perturbations and fragility value
+    """
+    vfm = []
+    vpm = []
+    
+    vfbp = []
+    vpbp = []
+    
+    rango=np.arange(0.1, 3.0, 0.1)
+    for K in tqdm(rango):
+        vf = []
+        vp = []
+        for i in range(runs):
+            red.CreateNet(K, N, p)
+            f=red.antifragile(T, runs=100)
+            vf.append(np.amin(f))
+            vp.append(np.argmin(f))
+        vfm.append(np.mean(vf))
+        vpm.append(np.mean(vp))
+        
+        vfbp.append(vf)
+        vpbp.append(vp)
+        
+    plt.plot(rango, vfm)
+    plt.ylabel("Fragility")
+    plt.xlabel("K")
+    plt.show()   
+    plt.plot(rango, vpm)
+    plt.ylabel("Perturbations")
+    plt.xlabel("K")
+    plt.show() 
+    
+    plt.boxplot(vfbp)
+    plt.ylabel("Fragility")
+    plt.xlabel("K")
+    plt.show()
+    plt.boxplot(vpbp)
+    plt.ylabel("Perturbations")
+    plt.xlabel("K")
+    plt.show() 
+    
     return
     
 if __name__ == '__main__':
@@ -245,33 +309,41 @@ if __name__ == '__main__':
     K=2.0
     N=100
     p=0.5
-    T=200
+    T=40
     
-    X=5 # how many perturbations
+    X=20 # how many perturbations
     O=1 # how often the perturbations take place
     
     red=RBN()
-    red.CreateNet(K, N, p)
-#    print(red.Con)
-#    print(red.Bool)
-#    red.RBNSort()
     
-    plots(red, T, X, O)
-    f=red.antifragile(T, runs=100)
-    plt.plot(f)
-    plt.ylabel("Fragility")
-    plt.xlabel("Perturbations")
-    plt.show()   
+    minAntifragile(red)
     
-    A=red.Attractors(T, runs=1000)
-    print("\nAttractores: ")
-    print(len(A))
-    edos=0
-    for i in A:
-        edos+=len(i)
-    print("Longitud promedio de Attractores: ")
-    print(edos/len(A))
-    if(edos!= 0):
-        print(str(len(A)/(edos)*100)+"%")
+    
+# =============================================================================
+#     red.CreateNet(K, N, p)
+#     print(red.Con)
+#     print(red.Bool)
+#     red.RBNSort()
+#     
+#     plots(red, T, X, O)
+#     f=red.antifragile(T, runs=100)
+#     plt.plot(f)
+#     plt.ylabel("Fragility")
+#     plt.xlabel("Perturbations")
+#     plt.show()   
+#     
+#     A=red.Attractors(T, runs=1000)
+#     print("\nAttractores: ")
+#     print(len(A))
+#     edos=0
+#     for i in A:
+#         edos+=len(i)
+#     print("Longitud promedio de Attractores: ")
+#     print(edos/len(A))
+#     if(edos!= 0):
+#         print(str(len(A)/(edos)*100)+"%")
+#         
+#     FR=plotAntifragile(N, p, T)
+# =============================================================================
     
     print("--- %s seconds ---" % (time.time() - start_time))
